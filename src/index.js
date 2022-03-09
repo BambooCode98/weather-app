667c38483f369b85295b803fca9cc9d00a3a7d86
BambooCode98 <jw.thomas98@yahoo.com>
2022-03-08 18:46:39 -0600

    adding forecast boxes to weather page next

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 'use strict';
 
-import './modules/domStuff.js';
+import './modules/weatherPage.js';
 import './modules/geoAPI.js';
 import './modules/weatherAPI.js';
 import './style.css';
 
 import { getCityCoords } from './modules/geoAPI';
 import { getWeatherData } from './modules/weatherAPI';
+import { createDays } from './modules/weatherPage';
 
 let coordinates = [];
+let forecastDays = [];
 let lat, long;
-let location, state, country;
-location = "Vergennes";
-state = "IL";
-country = "US";
+// let location, state, country;
+// const country = form.querySelector('input[id="country"]').value;
+const country = 'US';
+let d0, d0n, d1, d1n, d2, d2n, d3, d3n, d4, d4n, d5, d5n;
+let x, y;
+const form = document.forms['formID'];
 
-//event here
 
-getCityCoords(location, state, country)
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const location = form.querySelector('input[id="location"]').value;
+  const state = form.querySelector('input[id="state"]').value;
+  
+  getCityCoords(location, state, country)
   .then( values => {
-    let x, y;
     [x, y] = values;
     coordinates.push(x, y);
     console.log(coordinates);
@@ -29,10 +36,19 @@ getCityCoords(location, state, country)
     long = coordinates[1];
     console.log(lat, long);
     getWeatherData(lat, long)
-      .then( (days) => {
-        console.log(days[0].temperature);
-      })
+    .then( (days) => {
+      [d0, d0n, d1, d1n, d2, d2n, d3, d3n, d4, d4n, d5, d5n] = days;
+      forecastDays.push(d0, d0n, d1, d1n, d2, d2n, d3, d3n, d4, d4n, d5, d5n);
+      console.log(forecastDays);
+      // createForecastPage(forecastDays);
+      coordinates = [];
+      forecastDays = [];
+    })
   })
+  
+  
+})
+
 
 
 
