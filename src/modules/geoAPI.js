50692586af2fcde040e8959cb864c2fd7d7255e3
BambooCode98 <jw.thomas98@yahoo.com>
2022-03-09 21:12:31 -0600

    changed a link to https so it would work from gh pages deployment.

--- src/modules/geoAPI.js
+++ src/modules/geoAPI.js
@@ -107,7 +107,7 @@ export async function getCityCoords(location, state, country) {
 
   let lat;
   let long;
-  const city = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location},${state},${country}&appid=daa0efcefa043d17b3c66f5e04fe9bd7`, {mode: 'cors'});
+  const city = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location},${state},${country}&appid=daa0efcefa043d17b3c66f5e04fe9bd7`, {mode: 'cors'});
   const cityData = await city.json();
   lat =  Math.round(cityData[0].lat * 100) / 100;
   long = Math.round(cityData[0].lon * 100) / 100;
